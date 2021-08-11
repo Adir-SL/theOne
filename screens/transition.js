f1913dfd7c6b3951c66722e52dda722945891208
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-11 22:11:59 +0300

    added link trigger

--- screens/transition.js
+++ screens/transition.js
@@ -1,24 +1,30 @@
 window.addEventListener('load', function () {
-//   this.alert(document.body.innerHTML);
-var frame = document.createElement("iframe");
-frame.src="screen_02.html";
-frame.style.position = "absolute";
-frame.style.width = "100%";
-frame.style.height = "100%";
-frame.style.margin = "0";
-frame.style.top = "0";
-frame.style.left = "0";
-frame.style.border = "0";
-frame.style.zIndex = "99";
-frame.style.opacity = "0";
-frame.style.pointerEvents = "none";
-frame.style.transition = "all 300ms linear";
-frame.id = "nextFrame";
-this.document.body.appendChild(frame);
+    var x = document.getElementsByTagName("a");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        var frame = document.createElement("iframe");
+        frame.src = x[i].href;
+        frame.style.position = "absolute";
+        frame.style.width = "100%";
+        frame.style.height = "100%";
+        frame.style.margin = "0";
+        frame.style.top = "0";
+        frame.style.left = "0";
+        frame.style.border = "0";
+        frame.style.zIndex = 99 + i;
+        frame.style.opacity = "0";
+        frame.style.pointerEvents = "none";
+        frame.style.transition = "all 300ms linear";
+        frame.id = "nextFrame_" + i;
+        this.document.body.appendChild(frame);
+
+        x[i].onclick="openLink("+x[i].href+")";
+        x[i].href="";
+    }
 })
 
 function openLink(x){
     // window.loaction = x;
-    document.getElementById("nextFrame").style.opacity = "1";
+    document.getElementById("nextFrame_").style.opacity = "1";
     setTimeout(function(){ window.location = x; }, 300);
 }
