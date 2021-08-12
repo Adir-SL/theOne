141ac9e39b6390f53b27209d9c32c16cef7f920d
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-12 08:29:55 +0300

    Update transition.js

--- screens/transition.js
+++ screens/transition.js
@@ -1,17 +1,6 @@
-// function openLink(frame, url) {
-//     fetch("screen_02.html" /*, options */)
-//     .then((response) => response.text())
-//     .then((html) => {
-//         frame.innerHTML = html;
-//     })
-//     .catch((error) => {
-//         console.warn(error);
-//     });
-// } 
-
 function fadeLink(url){
     document.getElementsByClassName("wrapper")[0].style.opacity = 0;
-    setTimeout(function(){ window.location = url; }, 300);
+    setTimeout(function(){ window.location.href = url; }, 300);
 }
 
-// setTimeout(function(){ document.getElementsByClassName("wrapper")[0].style.opacity = 1; }, 10);
+setTimeout(function(){ document.getElementsByClassName("wrapper")[0].style.opacity = 1; }, 10);
