17abd2090896e66dee01e0cd24f7873d6f1771a2
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-12 16:47:10 +0300

    Font change

--- screens/transition.js
+++ screens/transition.js
@@ -1,10 +1,6 @@
 function fadeLink(url){
-    // document.getElementsByClassName("wrapper")[0].classList.add("fadeMeOut");
-    // document.getElementsByClassName("wrapper")[0].style.opacity = "0";
     setTimeout(function(){ window.location.href = url; }, 300);
 }
-
-// setTimeout(function(){ document.getElementsByClassName("wrapper")[0].style.opacity = 1; }, 100);
 
 window.addEventListener("beforeunload", function(event) { document.getElementsByClassName("wrapper")[0].style.opacity = "0";});
 window.addEventListener("load", function(event) { document.getElementsByClassName("wrapper")[0].style.opacity = "1";});
