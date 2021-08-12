d297f6f78dbc8d7111df2ca8ba7b8ba9deba5392
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-12 07:56:02 +0300

    Transition fix

--- screens/transition.js
+++ screens/transition.js
@@ -1,42 +1,10 @@
-window.addEventListener('load', function () {
-    var x = document.getElementsByClassName("transjs");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        var frame = document.createElement("iframe");
-        frameTest = x[i].onclick.toString();
-        frame.src = frameTest = frameTest.slice(frameTest.lastIndexOf("(")+2, frameTest.lastIndexOf(",")-1);
-        frame.style.position = "absolute";
-        frame.style.width = "100%";
-        frame.style.height = "100%";
-        frame.style.margin = "0";
-        frame.style.top = "0";
-        frame.style.left = "0";
-        frame.style.border = "0";
-        frame.style.zIndex = 99 + i;
-        frame.style.opacity = "0";
-        frame.style.pointerEvents = "none";
-        frame.style.transition = "all 300ms linear";
-        frame.id = "nextFrame_" + i;
-        this.document.body.appendChild(frame);
-
-        // x[i].setAttribute("onclick","openLink('screen_02.html', event);");
-        x[i].id = "linkNum_" + i;
-        
-    }
-})
-
-function openLink(x,e){
-    // window.loaction = x;
-    tempTest = e.target.id;
-    tempTest = tempTest.slice(tempTest.lastIndexOf("_")+1);
-    document.getElementById("nextFrame_"+tempTest).style.opacity = "1";
-    setTimeout(function(){ 
-        document.getElementById("nextFrame_"+tempTest).style.pointerEvents = "all";
-        document.getElementById("nextFrame_"+tempTest).style.overflow = "auto";
-        alert("Y")
-    }, 300);
-    // setTimeout(function(){ window.location = x; }, 300);
-    
-    // document.getElementById("nextFrame_").style.opacity = "1";
-    // setTimeout(function(){ window.location = x; }, 300);
-}
+function openLink(url) {
+    fetch("screen_02.html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.body.innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+} 
