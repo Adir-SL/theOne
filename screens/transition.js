e029c0f7b040903966a5f2a570a0bdec3f22a400
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-12 08:36:49 +0300

    Update transition.js

--- screens/transition.js
+++ screens/transition.js
@@ -1,6 +1,7 @@
 function fadeLink(url){
-    document.getElementsByClassName("wrapper")[0].classList.add("fadeMeOut");
+    // document.getElementsByClassName("wrapper")[0].classList.add("fadeMeOut");
+    document.getElementsByClassName("wrapper")[0].style.opacity = "0";
     setTimeout(function(){ window.location.href = url; }, 300);
 }
 
-setTimeout(function(){ document.getElementsByClassName("wrapper")[0].style.opacity = 1; }, 10);
+setTimeout(function(){ document.getElementsByClassName("wrapper")[0].style.opacity = 1; }, 50);
