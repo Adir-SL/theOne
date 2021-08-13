38b714241489a822b8ac95d0efad8d6fe0f28e58
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-13 06:34:34 +0300

    Transition fixes

--- screens/transition.js
+++ screens/transition.js
@@ -1,6 +1,20 @@
-function fadeLink(url){
+/*function fadeLink(url){
     setTimeout(function(){ window.location.href = url; }, 300);
 }
 
 window.addEventListener("beforeunload", function(event) { document.getElementsByClassName("wrapper")[0].style.opacity = "0";});
 window.addEventListener("load", function(event) { document.getElementsByClassName("wrapper")[0].style.opacity = "1";});
+*/
+
+function openLink (e) {
+    (e || window.event).preventDefault();
+
+    fetch("screen_02.html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.body.innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+} 
