7b868db97502d8e564c8f1e58f1e0b261c8852c7
Adir <slutzkis@gmail.com>
2021-08-23 03:34:26 +0300

    Update script.js

--- screens/script.js
+++ screens/script.js
@@ -23,6 +23,7 @@ function crossClick(){
     whatPageSlice = whatPage.slice(whatPage.indexOf("_"), whatPage.indexOf("."));
     if(document.getElementById('moreData').className == "openCross"){
         document.getElementsByClassName("flexDiv")[0].style.backgroundImage = "url(story"+whatPageSlice+".png)";
+        document.getElementById("svgLayer").data = "story"+whatPageSlice+".svg";
         document.getElementsByClassName("storyDiv")[0].style.opacity = ".6";
         // alert("url(story"+whatPageSlice+".svg)")
     }else{
