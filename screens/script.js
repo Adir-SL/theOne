2bb8608ef1296e1547d5b1c08ca6d6ce596569b0
Adir <slutzkis@gmail.com>
2021-08-25 01:00:25 +0300

    Update script.js

--- screens/script.js
+++ screens/script.js
@@ -10,8 +10,6 @@ function buttonSelect(e){
 function resetMenu(){
     document.getElementById('storyMenu').style.transform='translateY(120%)';
     document.getElementById('dataMenu').style.transform='translateY(120%)';
-    // document.getElementById('firstMenuButton').classList.remove("blackText");
-    // document.getElementById('secondMenuButton').classList.remove("blackText");
 }
 
 function addCross(){
