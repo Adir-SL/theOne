2b5d9adf5015f7381407bf0162cb5beee446a429
Adir <slutzkis@gmail.com>
2021-08-25 01:00:47 +0300

    Update transition.js

--- screens/transition.js
+++ screens/transition.js
@@ -1,11 +1,3 @@
-/*function fadeLink(url){
-    setTimeout(function(){ window.location.href = url; }, 300);
-}
-
-window.addEventListener("beforeunload", function(event) { document.getElementsByClassName("wrapper")[0].style.opacity = "0";});
-window.addEventListener("load", function(event) { document.getElementsByClassName("wrapper")[0].style.opacity = "1";});
-*/
-
 function openLink (e, url) {
     (e || window.event).preventDefault();
     window.whatPage = url;
