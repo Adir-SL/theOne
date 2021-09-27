80e3fc4978557913519b9371c66373973cb410d2
Adir <slutzkis@gmail.com>
2021-09-27 12:14:48 +0300

    Update transition.js

--- screens/transition.js
+++ screens/transition.js
@@ -27,6 +27,37 @@ function openLink (e, url) {
 
     crossReset();
 }
+
+function openFromFrame (e, url) {
+    (e || window.event).preventDefault();
+    window.whatPage = url;
+    
+    fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+        parent.document.getElementsByClassName("wrapper")[0].innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+
+    parent.document.getElementsByClassName("footer")[0].style.opacity = "1";
+    parent.document.getElementsByClassName("footer")[0].style.pointerEvents = "all";
+
+    window.scrollTo(0,0);
+
+    setTimeout(function(){
+        document.getElementById("svgLayer").style.transition = "opacity 1ms linear 0s";
+        document.getElementById("svgLayer").style.opacity = "0";
+        document.getElementById("svgLayer").style.pointerEvents = "none";
+        window.whatPageSlice = window.whatPage.slice(window.whatPage.indexOf("_"), window.whatPage.indexOf("."));
+        document.getElementById("svgLayer").data = "story"+whatPageSlice+".svg";
+        document.getElementById("svgLayer").style.transition = "opacity 300ms linear 0s";
+    }, 300);
+
+    parent.crossReset();
+}
+
 function openCloth(x){
     document.getElementById("clothFrame").src = x;
     document.getElementById("clothFrame").style.pointerEvents = "all";
